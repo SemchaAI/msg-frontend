ef0d6e96f55200ead51081bf12f2a922ef3c5d33
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-03-06 19:27:36 +0200

    +swithingSVG
    different svg for light and dark themes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./login.module.css";
 import { ReactComponent as SurfingImg } from "./images/surfing.svg";
+import { ReactComponent as SurfingImgDark } from "./images/surfingDark.svg";
 import { FormLogin } from "../../entities/LoginForm/ui/FormLogin";
+import { useTheme } from "../../shared/theme/hooks/useTheme";
 
 const Login = () => {
+  const [currentTheme, _] = useTheme();
   return (
     <div className={styles.login}>
-      <SurfingImg className={styles.img} width="279px" height="194px" />
+      {currentTheme === "light" ? (
+        <SurfingImg className={styles.img} width="279px" height="194px" />
+      ) : (
+        <SurfingImgDark className={styles.img} width="279px" height="194px" />
+      )}
       <FormLogin />
     </div>
   );
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { FormRegister } from "../../entities/RegisterForm/ui/FormRegister";
 import styles from "./register.module.css";
 import { ReactComponent as LoginImg } from "./images/Global.svg";
+import { ReactComponent as LoginImgDark } from "./images/DarkGlobal.svg";
+import { useTheme } from "../../shared/theme/hooks/useTheme";
 
 const Register = () => {
+  const [currentTheme, _] = useTheme();
   return (
     <div className={styles.login}>
-      <LoginImg className={styles.img} width="375px" height="281px" />
+      {currentTheme === "light" ? (
+        <LoginImg className={styles.img} width="375px" height="281px" />
+      ) : (
+        <LoginImgDark className={styles.img} width="375px" height="281px" />
+      )}
       <FormRegister />
     </div>
   );
