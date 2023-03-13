68f8f0f8d0fae31124e5bc597d748359447d7fdf
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-03-13 11:12:55 +0200

    +-profile
    now  userInfo component getting id prop

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Header } from "../../entities/Header/Header";
 import { Footer } from "../../entities/Nav(Footer)/Footer";
 import { UserInfo } from "../../entities/UserInfo/UserInfo";
 
 import styles from "./profile.module.css";
 
-const Profile = () => {
+const Profile = ({ route, navigation }) => {
+  const id = useLocation();
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <UserInfo />
+        <UserInfo idObj={id?.state?.id} />
       </main>
       <Footer />
     </>
