1f458aee3b51ae1f825664e8033b41f173c1b8e0
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-03-13 11:10:34 +0200

    +-Profile page
    updated profile page with new custom hook, if you check your own page you can see friends requests , and can change status or avatar, in another way you can only check user Info

--- src/entities/UserInfo/UserInfo.jsx
+++ src/entities/UserInfo/UserInfo.jsx
@@ -1,154 +1,128 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectUser, setAll } from "../redux/user";
-
 import styles from "./userInfo.module.css";
 
 import { ReactComponent as Add } from "./images/add.svg";
 import { ReactComponent as Decline } from "./images/decline.svg";
 
-import { ReactComponent as Avatar } from "./images/avatar.svg";
+import cn from "classnames";
+import { Avatar } from "../../features/avatar/Avatar";
+import { FormStatus } from "../../features/formStatus/FormStatus";
+import { useMeProfile } from "./hooks/useMeProfile";
 import { userApi } from "../services/userServices";
-import { API_URL } from "../../shared/config";
-export const UserInfo = () => {
-  const dispatch = useDispatch();
-  const User = useSelector(selectUser);
 
-  // const [avatar, setAvatar] = useState();
-
-  const inputFileRef = useRef();
-
-  const [uploadAvatar, { error }] = userApi.useUploadImageUserMutation();
-  const [changeAvatar, { error: avatarError }] =
-    userApi.useChangeAvatarUserMutation();
-  const [deleteOldAvatar, { error: deleteAvatar }] =
-    userApi.useDeleteOldAvatarMutation();
-  const [updateAvatars, { error: updAva }] = userApi.useUpdateAvatarsMutation();
-  const {
+export const UserInfo = ({ idObj = "CurrentUser" }) => {
+  //important feature, but firstly need to refactor all component  ////upd. 80%
+  // example of data
+  // {
+  //   id: "63f9ea00d9a1b612d502efbd",
+  // }
+  const [
     data,
-    isLoading,
-    error: getMeError,
-    refetch,
-  } = userApi.useGetMeQuery(null, { pollingInterval: 5000 });
+    inputFileRef,
+    changeFileHandler,
+    setStatusFlag,
+    statusFlag,
+    clickHandlerAdd,
+    clickHandlerRemove,
+  ] = useMeProfile(idObj);
 
-  const [removeFriendReq, { error: removeErr }] =
-    userApi.useRemoveFriendReqMutation();
-  const [addFriendReq, { error: addErr }] = userApi.useAddFriendReqMutation();
-
-  const clickHandlerRemove = async (id) => {
-    await removeFriendReq(id);
-    refetch();
-  };
-  const clickHandlerAdd = async (id) => {
-    await addFriendReq(id);
-    refetch();
-  };
-
-  useEffect(() => {
-    dispatch(setAll(data));
-  }, [data, dispatch, changeAvatar]);
-
-  const changeFileHandler = async (event) => {
-    try {
-      const formData = new FormData();
-      const file = event.target.files[0];
-      formData.append("image", file);
-      const { data } = await uploadAvatar(formData);
-      // console.log(data);
-      // console.log(data.url);
-      const currLink = {
-        url: data.url,
-      };
-      await changeAvatar(currLink);
-      await updateAvatars(currLink);
-      if (User.avatarUrl && User.avatarUrl !== "/uploads/avatar.svg") {
-        const oldlink = {
-          linkImg: User.avatarUrl,
-        };
-        await deleteOldAvatar(oldlink);
-      }
-      refetch();
-    } catch (error) {
-      //  console.log(error);
-    }
-  };
   return (
     <>
-      <section className={styles.allAboutUser}>
-        {data && (
-          <>
-            <div className={styles.personalInfo}>
-              <img
-                className={styles.img}
-                src={API_URL + data.avatarUrl}
-                width="96px"
-                height="96px"
-                onClick={() => inputFileRef.current.click()}
-                alt="default avatar"
-              ></img>
-              <input
-                ref={inputFileRef}
-                onChange={changeFileHandler}
-                type={"file"}
-                accept="image/x-png,image/gif,image/jpeg"
-                hidden
-              ></input>
-              <div className={styles.userInfo}>
-                <h3>{data.nickname}</h3>
-                <h3 className={`${styles.email}`}>{data.email}</h3>
-                <h3 className={styles.text}>
-                  Status: <p>Some day, dev will add this posibility</p>
-                </h3>
-              </div>
-            </div>
-          </>
-        )}
-      </section>
-      {/* <div className="inProcess">Here will be some settings in future</div> */}
-      {data && data.friendsReq ? (
+      {data && (
         <>
-          <h3 className={styles.listH3}>Friend requests</h3>
-          <ul className={styles.listContainer}>
-            {data.friendsReq.map((val, index) => (
-              <li key={val._id} className={styles.listCard}>
-                <div className={styles.listCardBlock}>
-                  <img
-                    className={styles.avatar}
-                    src={API_URL + val.avatarUrl}
-                    height={"40px"}
-                    width={"40px"}
-                    alt="avatar"
-                  ></img>
-                  <div className={styles.cardInfo}>
-                    <div className={styles.nickname}>{val.nickname}</div>
-                    <div>{val.email}</div>
-                  </div>
-                </div>
-                {/* {!data.friends.map((el) => el._id.includes(data.friends._id))
-                  .length ? ( */}
+          <section className={styles.allAboutUser}>
+            <>
+              <div className={styles.personalInfo}>
+                {data.flag === false ? (
+                  <>
+                    <Avatar data={data} size={95} />
+                    <div className={styles.userInfo}>
+                      <h3>{data.nickname}</h3>
+                      <h3 className={`${styles.email}`}>{data.email}</h3>
+                      <h3 className={styles.text}>
+                        Status: <p>{data.status}</p>
+                      </h3>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <Avatar data={data} size={95} upload={inputFileRef} />
+                    <input
+                      ref={inputFileRef}
+                      onChange={changeFileHandler}
+                      type={"file"}
+                      accept="image/x-png,image/gif,image/jpeg,image/svg+xml"
+                      hidden
+                    ></input>
+                    <div className={styles.userInfo}>
+                      <h3>{data.nickname}</h3>
+                      <h3 className={`${styles.email}`}>{data.email}</h3>
+                      {statusFlag ? (
+                        <h3
+                          className={styles.text}
+                          onClick={() => setStatusFlag(!statusFlag)}
+                        >
+                          Status: <p>{data.status}</p>
+                        </h3>
+                      ) : (
+                        <FormStatus
+                          status={data.status}
+                          func={setStatusFlag}
+                          flag={statusFlag}
+                        />
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
+            </>
+          </section>
+          {data.flag && (
+            <>
+              {/* <div className="inProcess">Here will be some settings in future</div> */}
+              {data.friendsReq ? (
                 <>
-                  <button
-                    onClick={() => clickHandlerAdd(val._id)}
-                    className={styles.btnAdd}
-                  >
-                    <Add />
-                  </button>
-                  <button
-                    onClick={() => clickHandlerRemove(val._id)}
-                    className={styles.btnDecline}
-                  >
-                    <Decline />
-                  </button>
-                </>
-                {/* ) : (
+                  <h3 className={styles.listH3}>Friend requests</h3>
+                  <ul className={styles.listContainer}>
+                    {data.friendsReq.map((val, index) => (
+                      <li key={val._id} className={styles.listCard}>
+                        <div className={styles.listCardBlock}>
+                          <Avatar data={val} size={40} />
+                          <div className={styles.cardInfo}>
+                            <div className={styles.nickname}>
+                              {val.nickname}
+                            </div>
+                            <div>{val.email}</div>
+                          </div>
+                        </div>
+                        {/* {!data.friends.map((el) => el._id.includes(data.friends._id))
+                  .length ? ( */}
+                        <>
+                          <button
+                            onClick={() => clickHandlerAdd(val._id)}
+                            className={styles.btnAdd}
+                          >
+                            <Add />
+                          </button>
+                          <button
+                            onClick={() => clickHandlerRemove(val._id)}
+                            className={styles.btnDecline}
+                          >
+                            <Decline />
+                          </button>
+                        </>
+                        {/* ) : (
                   <div className={styles.added}>Added</div>
                 )} */}
-              </li>
-            ))}
-          </ul>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                <div>No requests</div>
+              )}
+            </>
+          )}
         </>
-      ) : (
-        <div>No requests</div>
       )}
     </>
   );
