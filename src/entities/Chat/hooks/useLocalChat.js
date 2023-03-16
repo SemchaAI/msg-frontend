48ccc1d8737ec7d7ac753739ab75608df5a07422
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-03-16 16:02:38 +0200

    + userId
    now chat query consume userId

--- src/entities/Chat/hooks/useLocalChat.js
+++ src/entities/Chat/hooks/useLocalChat.js
@@ -4,11 +4,13 @@ import { userApi } from "../../services/userServices";
 
 export const useLocalChat = () => {
   const params = useParams();
+  //console.log(params.userId);
   const [page, setPage] = useState(1);
 
   const { data, isLoading, error, refetch } = userApi.useGetChatQuery(
     {
       id: params.id,
+      userId: params.userId,
       page,
     },
     { pollingInterval: 2000 }
